7c0444785c811383299c1dbf0c4f90d5cff775f5
Riyaz9399 <sakshikushwaha5460@gmail.com>
2024-10-02 00:52:15 +0530

    Adding CORS setup

--- Backend/index.js
+++ Backend/index.js
@@ -5,25 +5,32 @@ import router from "./routes/index.js";
 import connectDB from "../Backend/config/db.js";
 import cookieParser from "cookie-parser";
 
-
 dotenv.config();
 const app = express();
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
+
+// CORS configuration
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000']; // Include localhost for development
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL ,
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Allow credentials if needed
 }));
 
-app.use("/api",router);
+app.use("/api", router);
+
 const PORT = process.env.PORT || 8080;
-connectDB().then(()=>{
+connectDB().then(() => {
   app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
   });
-})
-
+});
