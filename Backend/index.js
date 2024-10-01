59d503975952d552a8b3183ab90eeb1dcffbb5d3
Riyaz9399 <sakshikushwaha5460@gmail.com>
2024-10-02 00:11:34 +0530

    ADD the FrontEnd Url

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000' ,
+  origin: process.env.FRONTEND_URL ,
   credentials: true
 }));
 
